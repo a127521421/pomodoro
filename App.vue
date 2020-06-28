ac1ca1339566d6b6f527c4fe7b46d03788f7dc40
a127521421 <a127521421@yahoo.com.tw>
2020-06-28 17:21:55 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/"><font-awesome-icon :icon="['fas', 'ethernet']"></font-awesome-icon></router-link>
+      <router-link to="/list"><font-awesome-icon :icon="['fas', 'bars']"></font-awesome-icon></router-link>
+      <router-link to="/settings"><font-awesome-icon :icon="['fas', 'cog']"></font-awesome-icon></router-link>
+    </div>
+    <keep-alive >
+      <router-view id="main" />
+    </keep-alive>
+  </div>
+</template>
+
+<style lang="stylus">
+#app {
+  height:100vh
+  text-align: center;
+  background-image: linear-gradient(to top, #96fbc4 0%, #f9f586 100%);
+}
+
+#main{
+  position relative
+  top 15%
+}
+
+#nav{
+  font-size:3rem;
+  position:relative;
+  top: 90%;
+  display:flex;
+  justify-content:space-around;
+  }
+</style>
---
+++ Settings.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="settings">
+    <b-table :items="items" :fields="fields" @row-clicked="selectAlarm">
+      <template v-slot:cell(preview)="data">
+        <!-- controls可以直接寫 -->
+        <!-- <audio :src="'./alarms/'+data.item.file"></audio> -->
+        <button @click="Play(data.item.file)"><font-awesome-icon :icon="['fas', 'play']" ></font-awesome-icon></button>
+      </template>
+      <template v-slot:cell(select)="data">
+        <font-awesome-icon v-if="data.item.file == alarm" :icon="['fas', 'check']"></font-awesome-icon>
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'settings',
+  data () {
+    return {
+      items: [
+        {
+          name: '鬧鈴',
+          file: 'alarm1.mp3'
+        },
+        {
+          name: 'yay',
+          file: 'yay.mp3'
+        }
+      ],
+      fields: [
+        {
+          key: 'name',
+          label: '名稱'
+        },
+        {
+          key: 'preview',
+          label: '預覽'
+        },
+        {
+          key: 'select',
+          label: '選擇'
+        }
+      ]
+    }
+  },
+  computed: {
+    alarm () {
+      return this.$store.getters.alarm
+    }
+  },
+  methods: {
+    selectAlarm (item) {
+      this.$store.commit('selectAlarm', item.file)
+    },
+    Play () {
+      const pla = new Audio()
+      pla.src = './alarms/data.item.file'
+      pla.play()
+    }
+  }
+}
+</script>
